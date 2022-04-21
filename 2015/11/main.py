1dc17ea1fa4b2bf3846991190a2781d8149dcc5c
Timotei <timoteim98@gmail.com>
2022-04-21 13:41:43 +0300

    2015:11
    Signed-off-by: Timotei <timoteim98@gmail.com>

--- 2015/11/main.py
+++ 2015/11/main.py
@@ -1,39 +1,36 @@
+import time
 
 
-def passIsValid(password):
-    pass
 
-def incrementPass(password):
-    result = password
-    l = len(result)
-    if result[l-1] != 'z':
-        print(1)
-        result = result[0:l-1] + chr(ord(result[l-1]) + 1)
-    elif result[l-2] != 'z':
-        print(2)
-        result = result[0:l-2] + chr(ord(result[l-2]) + 1) + 'a'
-    elif result[l-3] != 'z':
-        print(3)
-        result = result[0:l-3] + chr(ord(result[l-3]) + 1) + 'aa'
-    elif result[l-4] != 'z':
-        print(4)
-        result = result[0:l-4] + chr(ord(result[l-4]) + 1) + 'aaa'
-    elif result[l-5] != 'z':
-        print(5)
-        result = result[0:l-5] + chr(ord(result[l-5]) + 1) + 'aaaa'
-    elif result[l-6] != 'z':
-        print(6)
-        result = result[0:l-6] + chr(ord(result[l-6]) + 1) + 'aaaaa'
-    elif result[l-7] != 'z':
-        print(7)
-        result = result[0:l-7] + chr(ord(result[l-7]) + 1) + 'aaaaaa'
-    else:
-        print(8)
-        result = chr(ord(result[l-8]) + 1) + 'aaaaaaa'
-    return result
-
-
-word_list = ['a', 'a', 'a']
+# def incrementPass(password):
+#     result = password
+#     l = len(result)
+#     if result[l-1] != 'z':
+#         print(1)
+#         result = result[0:l-1] + chr(ord(result[l-1]) + 1)
+#     elif result[l-2] != 'z':
+#         print(2)
+#         result = result[0:l-2] + chr(ord(result[l-2]) + 1) + 'a'
+#     elif result[l-3] != 'z':
+#         print(3)
+#         result = result[0:l-3] + chr(ord(result[l-3]) + 1) + 'aa'
+#     elif result[l-4] != 'z':
+#         print(4)
+#         result = result[0:l-4] + chr(ord(result[l-4]) + 1) + 'aaa'
+#     elif result[l-5] != 'z':
+#         print(5)
+#         result = result[0:l-5] + chr(ord(result[l-5]) + 1) + 'aaaa'
+#     elif result[l-6] != 'z':
+#         print(6)
+#         result = result[0:l-6] + chr(ord(result[l-6]) + 1) + 'aaaaa'
+#     elif result[l-7] != 'z':
+#         print(7)
+#         result = result[0:l-7] + chr(ord(result[l-7]) + 1) + 'aaaaaa'
+#     else:
+#         print(8)
+#         result = chr(ord(result[l-8]) + 1) + 'aaaaaaa'
+#     return result
+# word_list = ['a', 'a', 'a']
 
 """
 0  - a >>> ord('a')=97
@@ -55,9 +52,7 @@ Z modulo 26
 def word_to_list(word):
     list = []
     for c in word:
-        # print(c, ord(c), ord(c) - ord('a'))
         list.append(ord(c) - ord('a'))
-    # print(list)
     return list
 
 
@@ -65,52 +60,82 @@ def list_to_word(list):
     word = ''
     for el in list:
         word += chr(el + ord('a'))
-    print(word)
     return word
 
 
 def increment_word_list(list):
-    # for i in range(len(list)):
-    #     print(list[i])
     transport = 0
-    if list[len(list)-1] == 25:
+    if list[-1] == 25:
         transport = 1
-    list[len(list)-1] = (list[len(list)-1] + 1) % 26
+    list[-1] = (list[-1] + 1) % 26
     for i in range(len(list)-2, -1, -1):
-        if transport:
-            list[i] = (list[i] + 1) % 26
-            if list[i] == 0:
-                transport = 1
-            else:
-                transport = 0
+        list[i] = (list[i] + transport) % 26
+        if transport == 1 and list[i] == 0:
+            transport = 1
         else:
-            if list[i] == 25:
-                transport = 1
-            else:
-                transport = 0
-        
-    # print(list)
+            transport = 0
     return list
 
-def main():
-    # input = 'vzbxkghb'
-    input = 'aaa'
 
-    list = word_to_list(input)
+
+groups = [chr(x + ord('a')) + chr(x + 1 + ord('a')) + chr(x + 2 + ord('a')) for x in range(24)]
+pairs = [chr(x + ord('a')) + chr(x + ord('a')) for x in range(26)]
+
+
+def passIsValid(password):
+    print('passIsValid: ', password)
+    # print(groups)
+    # print(pairs)
+
+    if ('i' in password) or ('o' in password) or ('l' in password):
+        print(1)
+        return False
     
-    for i in range(17576):
-        list_to_word(list)
+    # abc, bcd, cde, and so on, up to xyz
+    contains = False
+    for g in groups:
+        if g in password:
+            contains = True
+            break
+    if contains == False:
+        print(2)
+        return False
+    
+    # like aa, bb, or zz
+    count = 0
+    for p in pairs:
+        if p in password:
+            count += 1
+    if count < 2:
+        print(3)
+        return False
+
+    print('ok')
+    return True
+    
+
+def main():
+    input = 'vzbxkghb'
+    # part two
+    input = 'vzbxxyzz'
+    list = word_to_list(input)
+    iterations = 26**len(input)
+
+    start = time.time()    
+    for i in range(iterations):
+        # list_to_word(list) dureaza mai putin decat print(list)
         list = increment_word_list(list)
-        
+        if passIsValid(list_to_word(list)) == True:
+            break
+    print(f'total time:  {time.time() - start} seconds')
+    print(f'The next password is: {list_to_word(list)}')
+    
+    
 
-    # for i in range(208827064576):
-    #     for c in input:
-    #         print(c, end=' ')
-    #     print()
+    # for i in range(iterations):
     #     result = incrementPass(input)
-    #     # print(result)
+    #     print(result)
     #     input = result
-
 
 
 
